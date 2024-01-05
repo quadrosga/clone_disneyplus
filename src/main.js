0ee29703d0d67a8df86bfbf799d3a940c2c984e3
Gabriela Quadros_do_Amaral <quadros.ga@hotmail.com>
2024-01-05 18:51:32 -0300

    Criação do cabeçalho

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');
-    
+
+    const heroSection = document.querySelector('.hero');
+    const heroHeight = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function(){
+        const currentPosition = window.scrollY;
+
+        if(currentPosition < heroHeight) {
+            hideHeaderElements();
+        }
+        else {
+            showHeaderElements();
+        }
+    })
+
+    // Seção de atrações, programação das abas     
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(button) {
             const targetTab = button.target.dataset.tabButton;
@@ -12,11 +27,21 @@ document.addEventListener('DOMContentLoaded', function() {
             button.target.classList.add('shows__tabs__button--is-active');
         })
     }
-
+    // Seção FAQ, accordion (abrir e fechar ao clicar)
     for (let i = 0; i < questions.length; i++) {
         questions[i].addEventListener('click', toggleAnswer);
     }
 })
+
+function hideHeaderElements() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function showHeaderElements() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
 
 function toggleAnswer(element) {
     const classe = 'faq__questions__item--is--open';
